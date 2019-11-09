1ce0a9e0d7f43fcfa967fedda796415ba55efe83
AARON M <asmarcks@gmail.com>
2019-11-09 15:52:00 -0600

    make look kinda pretty

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import SayHelloBitch from "./sayHelloBitch";
 import BitchTweet from "./BitchTweet"
 import './App.css';
 
 function App() {
 
-  
+ const [isRed, setRed]= useState(false);
+ const [count, setCount]= useState(0);
+ const increment = () => {
+   setCount(count + 1);
+   setRed(true);
+ }
 
   return (
     <div className="app">
-      <SayHelloBitch/>
+      <button onClick={increment}>Increment</button>
+      
+      <SayHelloBitch />
       <br></br>
-      <BitchTweet name="Like, Totally" message="random bitch tweet" number= "5"/>
-      <BitchTweet name="Ass Butt" message="random bitch tweet" number= "5"/>
-      <BitchTweet name="The Simpsons" message="random bitch tweet" number= "5"/>
-      <BitchTweet name="Mamma-lick-a-boo-boo-daddy" message="random bitch tweet" number= "5"/>
-
+      <h1 className={isRed ? "red" : ""} >AHHHHHHHHHHH!</h1>
+      <BitchTweet name="Like, Totally" message="random bitch tweet" number= {count}/>
+      <BitchTweet name="Ass Butt" message="random bitch tweet" number= {count}/>
+      <BitchTweet name="The Simpsons" message="random bitch tweet" number= {count}/>
+      <BitchTweet name="Mamma-lick-a-boo-boo-daddy" message="random bitch tweet" number= {count}/>
     </div>
   )
 }
--- src/BitchTweet.js
+++ src/BitchTweet.js
@@ -2,11 +2,14 @@ import React from "react";
 import "./App.css";
 
 function bitchTweet(props){
+
+    
     return(
         <div className= "tweet">
             <h3>Name: {props.name}</h3>
             <p>Bitch Tweet: {props.message}</p>
             <h3>Number of likes: {props.number}</h3>
+            
         </div>
     )
 }
